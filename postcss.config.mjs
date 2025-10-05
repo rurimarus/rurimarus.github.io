6d990fc90008e306580fb982b48ea19320a786cd
risn <168991821+rurimarus@users.noreply.github.com>
2025-10-05 04:55:36 +0000

    fix: organize imports and format plugins

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,3 @@
-import postcssImport from "postcss-import";
-import postcssNesting from "tailwindcss/nesting/index.js";
-import tailwindcss from "tailwindcss";
-
 export default {
-  plugins: [
-    postcssImport,
-    postcssNesting,
-    tailwindcss,
-  ],
+	plugins: [postcssImport, postcssNesting, tailwindcss],
 };
-
